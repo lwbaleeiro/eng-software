7a8999cea9bbdc10224924cf12091e3a6b94eff0
Luan Baleeiro <dev.baleeiro@gmail.com>
2025-03-05 22:05:25 -0300

    initial project structure

---
+++ src/main/java/br/com/lwbaleeiro/eng_software/EngSoftwareApplication.java
@@ -0,0 +1,13 @@
+package br.com.lwbaleeiro.eng_software;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EngSoftwareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EngSoftwareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/lwbaleeiro/eng_software/EngSoftwareApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lwbaleeiro.eng_software;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EngSoftwareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
